9cb3ab107a5f4a4888c2052fa9a96a21e33db2e4
Lilydahamna <liliadahamna2000@gmail.com>
2024-02-08 22:25:40 -0500

    changes to preprocess method

--- preprocess.py
+++ preprocess.py
@@ -1,67 +1,63 @@
 import nltk
-from nltk.tokenize import word_tokenize
 from nltk.corpus import stopwords
-from nltk.stem import PorterStemmer
 import string
 import re
 import os
+from sklearn.feature_extraction.text import CountVectorizer
+from nltk.stem import SnowballStemmer
 
-# Initialize NLTK resources
-nltk.download('punkt')
-nltk.download('stopwords')
 
-# Initialize Porter Stemmer
-porter = PorterStemmer()
+# Porter2 is an improved version of Porter Stemmer
+stemmer = SnowballStemmer("english")
 
 markup_pattern = re.compile(r'<[^>]+>')
 
 # Initialize set of stopwords by combining provided stopwords with the ones in the NLTK resource
-file_path = 'StopWords.txt'
-with open(file_path, 'r', encoding='utf-8') as file:
+nltk.download('stopwords')
+with open('StopWords.txt', 'r', encoding='utf-8') as file:
     provided_stop_words = file.read().split()
-    
-stop_words = set(provided_stop_words)| set(stopwords.words('english'))
+stop = set(provided_stop_words) | set(stopwords.words('english'))
 
+documents = []
 processed_tokens = set()
+vectorizer = CountVectorizer(stop_words= list(stop), token_pattern=r'\b[a-z]+\b')
 
-def preprocess(document):
+def preprocess():
     
-    # Remove words enclosed within angle brackets 
-    document = re.sub(markup_pattern, '', document)
-    
-    # Tokenization: Split the document into words
-    tokens = word_tokenize(document.lower())  
-    
-    # Preprocess tokens
-    for token in tokens:
-        if token.isalpha():
-            # Remove stopwords and stem the word
-            if token not in stop_words:
-                stemmed_token = porter.stem(token)
-                processed_tokens.add(stemmed_token)
-                
+    X = vectorizer.fit_transform(documents)
 
-def preprocess_documents(folder_path):
+    # Get the vocabulary 
+    vocabulary = vectorizer.get_feature_names_out()
+    
+    # stem words
+    for word in vocabulary:
+     processed_tokens.add(stemmer.stem(word))
+
+
+def extract_documents(folder_path):
     # List all files in the folder
     files = os.listdir(folder_path)
     
     # Iterate through each file
     for file_name in files:
-        # Construct the full path to the file
+        
         file_path = os.path.join(folder_path, file_name)
         
-        # Read the contents of the file
         with open(file_path, 'r', encoding='utf-8') as file:
             document = file.read()
-        # Preprocess the document
-        preprocess(document)
+            # Remove words enclosed within angle brackets
+            document = re.sub(markup_pattern, '', document)
+            documents.append(document)
+            
+       
 
-# set folder path and preprocess documents
-folder_path = 'coll'
-preprocess_documents(folder_path)
+# prepare documents and preprocess 
+extract_documents("coll")
+preprocess()
 
-# Write tokens to new file
-output_file = 'Vocabulary.txt'
-with open(output_file, 'w', encoding='utf-8') as file:
+# add vocabulary to a new file
+with open('Vocabulary.txt', 'w', encoding='utf-8') as output_file:
     for token in processed_tokens:
-        file.write(token + '\n')
+        output_file.write(token + '\n')
+
+print(len(processed_tokens))
